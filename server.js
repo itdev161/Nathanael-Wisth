c5518843a1212cd118c4bb3de870a4dba1f586fa
Nathanael <wisthn@gmatc.matc.edu>
2020-09-30 13:28:15 -0500

    added API endpoint

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import connectDatabase from './config/db';
 
+//start express app
 const app = express();
 
+//connect database
 connectDatabase();
+
+//configure express
+app.use(express.json({extended: false}));
 
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 app.listen(3000, () => console.log('Express server running on port 3000'));

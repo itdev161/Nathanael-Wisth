0343dbf085e9533d9bb1916f3b37014e36b4c326
Nathanael <wisthn@gmatc.matc.edu>
2020-09-30 13:45:05 -0500

    User Registration API Endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult} from 'express-validator';
 
 //start express app
 const app = express();
@@ -14,9 +15,20 @@ app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(422).json({errors: errors.array()});
+        }else{
+            res.send(req.body);
+        }
 })
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
